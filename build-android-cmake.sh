9ae0a007a5c033b3bc21a1bb40e1b2018195f39f
Rocka <i@rocka.me>
2021-06-21 00:29:38 +0800

    Don't hardcode CMake and Android Platform verison.

--- build-android-cmake.sh
+++ build-android-cmake.sh
@@ -1,33 +1,53 @@
 #!/usr/bin/env bash
 
+CMAKE=""
+
 build_libintl() {
     echo ">>> Building fmt for $1"
-    "$ANDROID_SDK_ROOT"/cmake/3.18.1/bin/cmake -B build \
+    "$CMAKE" -B build \
         -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_ROOT"/build/cmake/android.toolchain.cmake \
         -DANDROID_ABI="$1" \
-        -DANDROID_NATIVE_API_LEVEL=21 \
+        -DANDROID_PLATFORM="$ANDROID_PLATFORM" \
+        -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX=out/libintl-lite/"$1" \
         .
-    "$ANDROID_SDK_ROOT"/cmake/3.18.1/bin/cmake --build build
-    "$ANDROID_SDK_ROOT"/cmake/3.18.1/bin/cmake --build build --target install
+    "$CMAKE" --build build
+    "$CMAKE" --build build --target install
 }
 
 if [ -z "${ANDROID_SDK_ROOT}" ]; then
-    echo "ANDROID_SDK_ROOT not set."
+    echo "ANDROID_SDK_ROOT should be set to Android SDK path."
     exit 1
 fi
 
 if [ -z "${ANDROID_NDK_ROOT}" ]; then
-    echo "ANDROID_NDK_ROOT not set."
+    echo "ANDROID_NDK_ROOT should be set to Android NDK path."
     exit 1
 fi
 
-if [ -z "${BUILD_ARCH}" ]; then
-    echo "BUILD_ARCH not set; can be a ',' separated list."
+if [ -z "${ANDROID_SDK_CMAKE_VERSION}" ]; then
+    echo "ANDROID_SDK_CMAKE_VERSION should be set to desired cmake version in \$ANDROID_SDK_ROOT/cmake. eg. 3.18.1"
     exit 1
 fi
 
-IFS=',' read -ra ARCH <<< "${BUILD_ARCH}"
+if [ -z "${ANDROID_PLATFORM}" ]; then
+    echo "ANDROID_PLATFORM should be set to minimum API level supported by the library. eg. 21"
+    exit 1
+fi
+
+if [ -z "${ANDROID_ABI}" ]; then
+    echo "ANDROID_ABI not set; can be a ',' separated list. eg. armeabi-v7a,arm64-v8a"
+    exit 1
+fi
+
+CMAKE="$ANDROID_SDK_ROOT/cmake/$ANDROID_SDK_CMAKE_VERSION/bin/cmake"
+
+if [ ! -f "$CMAKE" ]; then
+    echo "Cannot find cmake: '$CMAKE'"
+    exit 1
+fi
+
+IFS=',' read -ra ARCH <<< "${ANDROID_ABI}"
 
 for a in "${ARCH[@]}"; do
   build_libintl "$a"
